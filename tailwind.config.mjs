45ac7f77b2cad48ab3353dc48806f9f9b51ed1fc
Miv Anna Donkaewpoo Berg <mibe0002@stud.kea.dk>
2024-10-25 22:46:34 +0200

    font, index, mainlayout, schemamarkup, import every comoponent, favicon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        customFont: ['"campton-xb"', "sans-serif"],
+        customFont: ['"campton-sb"', "sans-serif"],
+        customFont: ['"oliver"', "serif"],
+        customFont: ['"tiempos"', "serif"],
+        customFont: ['"tiempos-italic"', "serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
+};
