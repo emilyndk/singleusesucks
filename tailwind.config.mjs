1ae7c578760dcca1a50d1ac63a9f7a02cb5d71d1
Miv Anna Donkaewpoo Berg <mibe0002@stud.kea.dk>
2024-10-25 23:36:07 +0200

    igang med farver indsættelse, igang med knapper, farver

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,18 @@ export default {
         customFont: ['"tiempos"', "serif"],
         customFont: ['"tiempos-italic"', "serif"],
       },
+
+      colors: {
+        /* Farver - skriv disse værdier: hvid, sort osv., når de skal bruges i HTML*/
+        hvid: "var(--vista-white-50)",
+        sort: "var(--wood-smoke-950)",
+        // ER IKKE DONE ENDNU!!!
+        // --cerise-red-600: #ff1054;
+        // --pine-green-700: #007c79;
+        // --pine-green-800: #066968;
+        // --wood-smoke-700: #4f4f4f;
+        // --wood-smoke-950: #1a1a1a;
+      },
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/container-queries")],
